7c38818ede9bb9aeae0cda2874a127e31b5c8c89
Jeremy Coleman <jeremy@coglite.com>
2021-12-14 04:00:11 -0500

    g

--- gulpfile.js
+++ gulpfile.js
@@ -34,29 +34,11 @@ gulp.task("tsformat", () => {
 
 
 
-gulp.task('js-compile', function () {
-  const closureCompiler = require('google-closure-compiler').gulp();
-  
-  return gulp.src('./src/js/**/*.js', {base: './'})
-      .pipe(closureCompiler({
-          compilation_level: 'SIMPLE',
-          warning_level: 'VERBOSE',
-          language_in: 'ECMASCRIPT6_STRICT',
-          language_out: 'ECMASCRIPT5_STRICT',
-          output_wrapper: '(function(){\n%output%\n}).call(this)',
-          js_output_file: 'output.min.js'
-        }, {
-          platform: ['native', 'java', 'javascript']
-        }))
-      .pipe(gulp.dest('./dist/js'));
-});
-
 gulp.task("minify", function () {
   let closure = require("@ampproject/rollup-plugin-closure-compiler");
   let { terser } = require("rollup-plugin-terser");
   let rollup = require("./tools/vinyl-rollup")
   return gulp
-    .src(["./src/**/*{.js,.jsx,.ts,.tsx}"])
     .src(["./src/**/*{.js,.jsx,.ts,.tsx}"])
     .pipe(typescript())
     //.pipe(babel(BABEL_CONFIG))
